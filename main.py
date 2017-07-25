3da8adacd6a9ac44b6055d058fec7f2648b56445
Reika98 <sjisantillan@gmail.com>
2017-07-25 11:27:11 +0800

    cloud storage

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from 	google.appengine.ext 		import 	ndb
 import 	jinja2
 import 	os
 
-import 	google.appengine.api.images
+import google.appengine.api.images
 import firebase_admin
 
 
@@ -22,55 +22,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 #INITIALIZE FIREBASE
-# import firebase_admin
-# from firebase_admin import credentials
-
-# cred = credentials.Certificate("path/to/serviceAccountKey.json")
-# firebase_admin.initialize_app(cred)
-
-class Images(ndb.Model):
-    image_key = ndb.BlobKeyProperty()
-
-
-class MainPage(webapp2.RequestHandler):
-
-	def get(self):
-		upload_url = blobstore.create_upload_url('/upload')
-		template = JINJA_ENVIRONMENT.get_template('test.html')
-		self.response.write(template.render().format(upload_url))
-
-
-class PhotoUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
-
-	def post(self):
-		upload = self.get_uploads('file')[0]
-		instance = Images(image_key=upload.key())
-		instance.put()
-		# self.redirect('/view_photo/%s' % upload.key())
-		self.redirect('/respond')
-		
-
-class ViewPhotoHandler(blobstore_handlers.BlobstoreDownloadHandler):
-
-    def get(self, photo_key):
-        if not blobstore.get(photo_key):
-            self.error(404)
-        else:
-            self.send_blob(photo_key)
-
-
-class jsonReturn(webapp2.RequestHandler):
-
-	def get(self):
-		self.response.headers['Content-Type'] = 'application/json'   
-		reply = {
-		  'tags': ['jacket', 'hoodie', 'black'],
-		  'payload': 'some var',
-		} 
-		self.response.out.write(json.dumps(reply))
-
-
-
 #FIREBASE
 from firebase_admin import credentials
 from firebase_admin import db
@@ -84,9 +35,8 @@ app = firebase_admin.initialize_app(cred, {
 })
 
 # As an admin, the app has access to read and write all data, regradless of Security Rules
-main_ref = db.reference('restricted_access/secret_document')
-main_db = 'path/'
-
+# main_ref = db.reference('restricted_access/secret_document')
+# main_db = 'path/'
 
 def push_data(document, data):
 	db_ref = main_ref.child(document)
@@ -100,6 +50,75 @@ def get_data(document, query):
 	ref_str = main_db + document
 	ref = db.reference(ref_str)
 	return ref.get()
+
+
+# GCLOUD STORAGE
+from google.cloud import storage
+import cloudstorage as gcs
+# Reference an existing bucket.
+BUCKET_NAME = 'fanapton.appspot.com'
+
+class Images(ndb.Model):
+    image_key = ndb.BlobKeyProperty()
+
+
+class MainPage(webapp2.RequestHandler):
+
+	def get(self):
+		# upload_url = blobstore.create_upload_url('/upload')
+		template = JINJA_ENVIRONMENT.get_template('test.html')
+		self.response.write(template.render())
+
+
+# class PhotoUploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+
+# 	def post(self):
+# 		upload = self.get_uploads('file')[0]
+# 		instance = Images(image_key=upload.key())
+# 		instance.put()
+# 		# self.redirect('/view_photo/%s' % upload.key())
+# 		self.redirect('/respond')
+
+class PhotoUploadHandler(webapp2.RequestHandler):
+
+	def post(self):
+		print "TO UPLOAD"
+		uploaded_file = self.request.POST.get("file")
+		uploaded_file_content = uploaded_file.file.read()
+		uploaded_file_filename = uploaded_file.filename
+		uploaded_file_type = uploaded_file.type
+
+		write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+		gcs_file = gcs.open(
+        "/" + BUCKET_NAME + "/" + uploaded_file_filename,
+        "w",
+        content_type=uploaded_file_type,
+        retry_params=write_retry_params
+	    )
+		gcs_file.write(uploaded_file_content)
+		gcs_file.close()
+		self.redirect('/respond')
+
+
+class ViewPhotoHandler(blobstore_handlers.BlobstoreDownloadHandler):
+
+    def get(self, photo_key):
+			print "VIEW NA BES"
+			if not blobstore.get(photo_key):
+				self.error(404)
+			else:
+				self.send_blob(photo_key)
+
+
+class jsonReturn(webapp2.RequestHandler):
+
+	def get(self):
+		self.response.headers['Content-Type'] = 'application/json'   
+		reply = {
+		  'tags': ['jacket', 'hoodie', 'black'],
+		  'payload': 'some var',
+		} 
+		self.response.out.write(json.dumps(reply))
 
 
 app = webapp2.WSGIApplication([
