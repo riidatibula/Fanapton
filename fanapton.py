390c72c86ce6cdbe5a9673f95acd5be56cf2efdd
rdatibula <riidatibula@gmail.com>
2017-08-04 23:33:31 +0800

    added authentication

--- fanapton.py
+++ fanapton.py
@@ -5,16 +5,36 @@ import os
 import webapp2
 import jinja2
 
+from google.appengine.api import users
+
 JINJA_ENVIRONMENT = jinja2.Environment(
   loader=jinja2.FileSystemLoader('templates'),
   extensions=['jinja2.ext.autoescape'],
   autoescape=True)
 
-class Main(webapp2.RequestHandler):
-	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('home.html')
-		self.response.write(template.render())
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+
+    user = users.get_current_user()
+
+    if user:
+      url = users.create_logout_url(self.request.uri)
+      url_linktext = 'logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'login'
+
+    context = {
+    	'user': user,
+    	'url': url,
+    	'url_linktext': url_linktext
+    }
+
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    self.response.write(template.render(context))
+
 
 app = webapp2.WSGIApplication([
-	('/', Main),
+	('/', MainPage),
 ], debug=True)
