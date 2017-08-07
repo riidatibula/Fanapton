da4a5784866e1e1e3fe1409be8d3ec8fa7413b51
rdatibula <riidatibula@gmail.com>
2017-08-07 15:19:04 +0800

    added some configuration

--- fanapton.py
+++ fanapton.py
@@ -7,6 +7,7 @@ import jinja2
 from models import Shop
 
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 JINJA_ENVIRONMENT = jinja2.Environment(
   loader=jinja2.FileSystemLoader('templates'),
@@ -44,7 +45,7 @@ class AddShop(webapp2.RequestHandler):
     user = users.get_current_user()
 
     if user:
-      url = users.create_logout_url(self.request.uri)
+      url = users.create_logout_url('/')
       url_linktext = 'logout'
     else:
       url = users.create_login_url(self.request.uri)
@@ -72,14 +73,51 @@ class AddShop(webapp2.RequestHandler):
 
     self.redirect('/')
 
-class DeleteShop(webapp2.RequestHandler):
-  def post(self):
-    template = JINJA_ENVIRONMENT.get_template('addShop.html')
+
+class ShopDetails(webapp2.RequestHandler):
+  def handle_requests(self, url_string):
+    
+    shop_key = ndb.Key(urlsafe=url_string)
+    shop = shop_key.get()
+
+    user = users.get_current_user()
+
+    if user:
+      url = users.create_logout_url('/')
+      url_linktext = 'logout'
+    else:
+      url = users.create_login_url(self.request.uri)
+      url_linktext = 'login'
+
+    context = {
+      'user': user,
+      'url': url,
+      'url_linktext': url_linktext,
+      'shop': shop
+    }
+
+    template = JINJA_ENVIRONMENT.get_template('shopDetails.html')
     self.response.write(template.render(context))
+
+  def post(self, url_string):
+    self.handle_requests(url_string)
+
+  def get(self, url_string):
+    self.handle_requests(url_string)
+
+
+class DeleteShop(webapp2.RequestHandler):
+  def post(self, url_string):
+
+    shop_key = ndb.Key(urlsafe=url_string)
+    shop_key.delete()
+
+    self.redirect('/')
 
 
 app = webapp2.WSGIApplication([
 	('/', MainPage),
   ('/addShop', AddShop),
-  ('/deleteShop', DeleteShop)
+  ('/shopDetails/(?P<url_string>[\w\-]+)', ShopDetails),
+  ('/deleteShop/(?P<url_string>[\w\-]+)', DeleteShop)
 ], debug=True)
